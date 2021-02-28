ea767d10ee63060b2a5c3c6bacb79e73787a2ac4
aayush-aryan <MY_NAME@example.com>
2021-03-01 03:14:55 +0530

    [aayush]added. a program for data script by using javaScriptExecutor

--- src/test/java/org/bridgelabz/SelectCalenderByJs.java
+++ src/test/java/org/bridgelabz/SelectCalenderByJs.java
@@ -1,12 +1,23 @@
 package org.bridgelabz;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class SelectCalenderByJs extends  Base{
     @Test
-    public void selectDate(){
+    public void selectDate() throws InterruptedException {
         driver.get("https://www.spicejet.com/");
-
-
+        WebElement date = driver.findElement(By.id("view_fulldate_id_1"));
+        Thread.sleep(2000);
+        String dateValue = "29-02-2021";
+        Thread.sleep(2000);
+        selectDate_ByJs(date,dateValue);
     }
+    public static void selectDate_ByJs(WebElement element, String dateValue){
+        JavascriptExecutor javascriptexecutor   = (JavascriptExecutor) Base.driver;
+        javascriptexecutor.executeScript("arguments[0].setAttribute('value','"+dateValue+"');",element);
+    }
+
 }
